d4f6eb14ea55e0597e689f19595900fb01590709
sgregersen <sorge@nanotech.dtu.dk>
2014-04-02 15:52:32 +0200

    Added a new hpp file for eigensystem:range.

---
+++ Eigensystem/eigensystem_range.hpp
@@ -0,0 +1,21 @@
+/*
+Header file for EigenSystem:range: 
+
+---
+Copyright (C) 2014, Søren Schou Gregersen <sorge@nanotech.dtu.dk>
+ */
+#ifndef _EIGENSYSTEM_RANGE_H_
+# define _EIGENSYSTEM_RANGE_H_
+
+namespace EigenSystem{
+
+class range{
+public:
+  range();
+  virtual ~range();
+
+  }
+
+}
+
+#endif                                                      // _EIGENSYSTEM_RANGE_H_
